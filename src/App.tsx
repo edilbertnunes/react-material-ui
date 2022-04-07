f2189b30d52097f2250b1c6ddc4f0c86fff71a2e
Edilbert Nunes <edilbert.nunes@gmail.com>
2022-04-07 20:48:15 -0300

    Aula 03

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
-    <div className="App">
-      Teste Criação Brand Develop
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
-}
-
+};
