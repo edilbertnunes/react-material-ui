b283257e0e1d2e56ad1648efba05b45af6dac45a
Edilbert Nunes <edilbert.nunes@gmail.com>
2022-04-09 08:22:19 -0300

    Ajuste Aula05

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Light } from "@mui/icons-material";
 import { ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
+
 import { AppRoutes } from "./routes";
-import { LightTheme } from "./shared/layouts";
+import { LightTheme } from "./shared/themes";
 
 export const App = () => {
   return (
