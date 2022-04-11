4709398310a910ad3021816a11d62a4b2fab858c
Edilbert Nunes <edilbert.nunes@gmail.com>
2022-04-11 16:19:25 -0300

    Aula 06

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,21 @@
 import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAppThemeContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
+
+    const {toggleTheme} = useAppThemeContext();
+
+
     return (
         <Routes>
             
-            < Route path="/pagina-inicial" element={<Button variant='contained' color='primary'>Teste</Button>} />
-            < Route path="*" element={<Navigate to="/pagina-inicial" />} />
+            < Route path="/pagina-inicial" 
+            element={<Button variant='contained' 
+            color='primary' 
+            onClick={toggleTheme}>Alterar Tema Tema</Button>} />
+            < Route path="*" 
+            element={<Navigate to="/pagina-inicial" />} />
         </Routes>
     )
 }
