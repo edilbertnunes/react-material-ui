d2ef73657c22454ba099911a155cd525aa95afd2
Edilbert Nunes <edilbert.nunes@gmail.com>
2022-04-11 18:24:00 -0300

    Aulas 07 e 08

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,19 +3,19 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAppThemeContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
 
-    const {toggleTheme} = useAppThemeContext();
-
-
-    return (
-        <Routes>
-            
-            < Route path="/pagina-inicial" 
-            element={<Button variant='contained' 
-            color='primary' 
-            onClick={toggleTheme}>Alterar Tema Tema</Button>} />
-            < Route path="*" 
-            element={<Navigate to="/pagina-inicial" />} />
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
+            Alterar Tema Tema
+          </Button>
+        }
+      />
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
+};
